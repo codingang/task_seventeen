5beb0e619dd00335430eab1c7a15d539a277999d
liujuping <1052228950@qq.com>
2016-03-24 16:25:48 +0800

    add the task 17

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /**
- * Created by hongbojing on 3/23/16.
+ * Created by wangtingdong on 16/3/21.
  */
 /* 数据格式演示
  var aqiSourceData = {
@@ -11,9 +11,32 @@
  }
  };
  */
+// 用于渲染图表的数据
+var chartData = {};
+
+// 记录当前页面的表单选项
+var pageState = {
+    nowSelectCity: -1,
+    nowGraTime: "day"
+};
+var aqiSourceData = {
+    "北京": randomBuildData(500),
+    "上海": randomBuildData(300),
+    "广州": randomBuildData(200),
+    "深圳": randomBuildData(100),
+    "成都": randomBuildData(300),
+    "西安": randomBuildData(500),
+    "福州": randomBuildData(100),
+    "厦门": randomBuildData(100),
+    "沈阳": randomBuildData(500)
+};
+var formGraTime=document.getElementById('form-gra-time');
+var citySelect=document.getElementById("city-select");
+var item;
+var text="";
 
 // 以下两个函数用于随机模拟生成测试数据
-function getDateStr(dat) {
+function getDateStr(dat) {          //获得dat的时间
     var y = dat.getFullYear();
     var m = dat.getMonth() + 1;
     m = m < 10 ? '0' + m : m;
@@ -26,79 +49,111 @@ function randomBuildData(seed) {
     var dat = new Date("2016-01-01");
     var datStr = '';
     for (var i = 1; i < 92; i++) {
-        datStr = getDateStr(dat);
+        datStr = getDateStr(dat);      //datStr ＝ 2016-01-01
+        /*
+         returnData = {
+         2016-01-01 : 随机数；
+         2016-01-02 ：随机数；
+         }
+         */
         returnData[datStr] = Math.ceil(Math.random() * seed);
-        dat.setDate(dat.getDate() + 1);
+        dat.setDate(dat.getDate() + 1);      //dat多一天
     }
     return returnData;
 }
 
-var aqiSourceData = {
-    "北京": randomBuildData(500),
-    "上海": randomBuildData(300),
-    "广州": randomBuildData(200),
-    "深圳": randomBuildData(100),
-    "成都": randomBuildData(300),
-    "西安": randomBuildData(500),
-    "福州": randomBuildData(100),
-    "厦门": randomBuildData(100),
-    "沈阳": randomBuildData(500)
-};
+//鼠标移上去的时候显示具体信息
+function hoverDetail(e){
+    var detail= e.childNodes[0];
+    detail.style.visibility='visible';
+}
 
+//鼠标移除时候隐藏信息
+function hideDetail(e){
+    var detail= e.childNodes[0];
+    detail.style.visibility='hidden';
+}
 
-// 用于渲染图表的数据
-var chartData = {};
-var chartArray = [];
-// 记录当前页面的表单选项
-var pageState = {
-    nowSelectCity: -1,
-    nowGraTime: "day"
-};
+//绑定事件函数
+function on(element,eventName,listener) {
+    if(element.addEventListener) {
+        element.addEventListener(eventName,listener,false);
+    }
+    else if(element.attachEvent) {
+        element.attachEvent('on'+eventName,listener);
+    }
+    else
+        element['on'+eventName]=listener;
+}
+
 
 /**
  * 渲染图表
  */
 function renderChart() {
-
+    var aqiChartWrap=document.getElementById('aqi-chart-wrap');
+    var color='';
+    console.log(chartData);
+    text='';
+    for(var item in chartData) {        //遍历每个chartData数据
+        color='rgb('+parseInt(256*Math.random())+','+
+            parseInt(256*Math.random())+','+parseInt(256*Math.random())+')';
+        text+='<div class="chat_item" onmouseover="hoverDetail(this)" onmouseout="hideDetail(this)"  style="height: '+
+            chartData[item]+'px;background-color:'+color+'">'+
+            '<span class="detail">date: '+item+'<br />num: '+chartData[item]+'</span>'+'</div>';
+    }
+    aqiChartWrap.innerHTML = text;
 }
 
 /**
  * 日、周、月的radio事件点击时的处理函数
  */
 function graTimeChange() {
-    // 确定是否选项发生了变化
+    var ipt=document.getElementsByTagName("input");
+    for(var i=0;i<ipt.length;i++) {
 
-    // 设置对应数据
+        // 确定是否选项发生了变化
+        if(ipt[i].checked && ipt[i].value!=pageState.nowGraTime) {
+            pageState.nowGraTime=ipt[i].value;
 
-    // 调用图表渲染函数
+            // 设置对应数据
+            initAqiChartData();
+        }
+    }
 }
 
 /**
  * select发生变化时的处理函数
  */
 function citySelectChange() {
-    // 确定是否选项发生了变化
+    // 确定是否选项发生了变化           option的点击只对未选项有效。
 
     // 设置对应数据
-
-    // 调用图表渲染函数
+    initAqiChartData();
 }
 
 /**
  * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
  */
 function initGraTimeForm() {
-
+    //radio 点击事件绑定
+    on(formGraTime,'click',graTimeChange);
+    //formGraTime.addEventListener('click',graTimeChange,false);
 }
-
 /**
  * 初始化城市Select下拉选择框中的选项
  */
 function initCitySelector() {
     // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
+    text='';
+    for(item in aqiSourceData) {
+        text += '<option>'+item+'</option>';
+        citySelect.innerHTML=text;
+    }
 
     // 给select设置事件，当选项发生变化时调用函数citySelectChange
-
+    on(citySelect,'change',citySelectChange);
+    //citySelect.addEventListener('change',citySelectChange,false);
 }
 
 /**
@@ -107,6 +162,62 @@ function initCitySelector() {
 function initAqiChartData() {
     // 将原始的源数据处理成图表需要的数据格式
     // 处理好的数据存到 chartData 中
+    chartData={};
+    var sum=0,i= 0,char={};
+    for(item in aqiSourceData) {
+        if(citySelect.value==item) {
+            char=aqiSourceData[item];
+        }
+    }
+    switch (pageState.nowGraTime){
+        case 'day':
+            chartData=char;
+            break;
+        case 'week':
+            sum=0;i=0;
+            var week=0;
+            for(item in char) {
+                //console.log(new Date(item).getDay());
+                sum+=char[item];
+                i++;
+                if(new Date(item).getDay()==6) {        //判断是否是周日
+                    week++;
+                    console.log(week);
+                    chartData['2016年第'+week+'周']=parseInt(sum/i);
+                    i=0;
+                    sum=0;
+                }
+            }
+            if(i!=0) {
+                week++;
+                chartData['2016年第'+week+'周']=parseInt(sum/i);
+            }
+            break;
+        case 'month':
+            sum=0;i=0;
+            var mouth=1;
+            for(item in char) {
+                var date=new Date(item);
+                //console.log(date.getMonth());
+                if(date.getMonth()!=mouth) {
+                    mouth=date.getMonth();
+
+                    if(sum!=0)
+                        chartData[date.getFullYear()+'-'+ (mouth ? ('0'+mouth) : mouth)]=parseInt(sum/i);
+                    sum=0;
+                    i=0;
+                }
+                sum+=char[item];
+                i++;
+            }
+            if(i!=0) {
+                mouth++;
+                chartData[date.getFullYear()+'-'+ (mouth ? ('0'+mouth) : mouth)]=parseInt(sum/i);
+            }
+            break;
+    }
+    // 调用图表渲染函数
+    renderChart();
 }
 
 /**
@@ -119,125 +230,3 @@ function init() {
 }
 
 init();
-// first: make a change to the prototype to get the nth week of a date
-Date.prototype.getWeek = function (dowOffset) {
-
-    dowOffset = typeof(dowOffset) == 'int' ? dowOffset : 0; //default dowOffset to zero
-    var newYear = new Date(this.getFullYear(),0,1);
-    var day = newYear.getDay() - dowOffset; //the day of week the year begins on
-    day = (day >= 0 ? day : day + 7);
-    var daynum = Math.floor((this.getTime() - newYear.getTime() -
-            (this.getTimezoneOffset()-newYear.getTimezoneOffset())*60000)/86400000) + 1;
-    var weeknum;
-    //if the year starts before the middle of a week
-    if(day < 4) {
-        weeknum = Math.floor((daynum+day-1)/7) + 1;
-        if(weeknum > 52) {
-            nYear = new Date(this.getFullYear() + 1,0,1);
-            nday = nYear.getDay() - dowOffset;
-            nday = nday >= 0 ? nday : nday + 7;
-            /*if the next year starts before the middle of
-             the week, it is week #1 of that year*/
-            weeknum = nday < 4 ? 1 : 53;
-        }
-    }
-    else {
-        weeknum = Math.floor((daynum+day-1)/7);
-    }
-    return weeknum;
-};
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-// second: get the element of the display area
-//var text = '<div class="month-cell" style="width: 33%; height: 100px; background-color: #000; position: relative; display: inline-block;"></div>';
-//document.getElementById('aqiChartResult').innerHTML = text;
-
-// third: declare the variables
-var dateValue = '', cityValue = '北京', cityMatchData = {};
-var dayValue = '', weekValue = '', monthValue = '', yearValue = '';
-
-// fourth: define the event function on the time element
-document.getElementById('form-gra-time').addEventListener("click", getDateAndCity);
-function getDateAndCity(cityValue){
-    //everytime you click on this, it should be initialized
-    var readyArray = [];// to store the data from the huge obj, and store as an array
-    // this is what is gonna happen after you clicking on the time component
-    dateValue = document.querySelector('input[name = "graTime"]:checked').value;
-    cityElement = document.getElementById('city-select');
-    cityValue = cityElement.options[cityElement.selectedIndex].value;
-    // get the data that matches the city you select
-    cityMatchData = aqiSourceData[cityValue];
-    //console.log('step 1: ' + dateValue + ' ' + cityValue);
-    //console.log('step 2: get the match city data from raw data');
-    //console.log(cityMatchData);
-    //console.log(Object.keys(cityMatchData).length);
-    for(item in cityMatchData){
-        // get the values of "year", "month", "day" from the property in the object
-        yearValue = item.slice(0,4);
-        monthValue = item.slice(5,7);
-        dayValue = item.slice(8,10);
-        // use the three values to generate a new date to get the number of which week it is on
-        var newDateValue = new Date(yearValue,monthValue-1,dayValue);
-        weekValue = newDateValue.getWeek();
-        // use the data piece you get to generate a new array whose elements are objects
-        readyArray.push({value:cityMatchData[item], year: yearValue, month: monthValue, day: dayValue, week: weekValue});
-
-    }
-    // get how many weeks are there in the data
-    var totalWeekAmount = Math.round(readyArray.length / 7);
-    // define a new array to store the data of the total number for each week
-    var readyWeekArray = new Array(totalWeekAmount);//for rendering week been selected
-
-    for(var i = 0; i <= totalWeekAmount; i++){
-        var sum = 0;
-        for(var j = 0; j<readyArray.length; j++){
-            if(i == readyArray[j].week){
-                sum += readyArray[j].value;
-            }
-        }
-        readyWeekArray[i] = sum;
-    }
-    console.log(readyWeekArray);
-    // use this "readyWeekArray" to render if "week" is been clicked
-
-    var totalMonthAmount = readyArray[readyArray.length - 1].month;
-    var readyMonthArray = new Array(totalMonthAmount);
-    for(var x = 0; x < totalMonthAmount; x++){
-        var sum2 = 0;
-        for(var y = 0; y<readyArray.length; y++){
-            if(x == (readyArray[y].month - 1)){
-                sum2 += readyArray[y].value;
-            }
-        }
-        readyMonthArray[x] = sum2;
-    }
-    console.log(readyMonthArray);
-
-    var renderTarget = document.getElementById('aqiChartResult');
-    var renderContent = '';
-    switch (dateValue){
-        case 'day':
-            for(var a = 0; a < readyArray.length; a++) {
-                renderContent+= '<div class="month-cell" style="position: relative; display: inline-block;width: ' + 100/(readyArray.length) + '%; height: '+ readyArray[a].value +'px; background-color:'+ getRandomColor() +';"></div>';
-            }
-            break;
-        case 'week':
-            for(var b = 0; b < readyWeekArray.length; b++) {
-                renderContent+= '<div class="month-cell" style="position: relative; display: inline-block;width: ' + 100/(readyWeekArray.length) + '%; height: '+ readyWeekArray[b]/10 +'px; background-color:'+ getRandomColor() +';"></div>';
-            }
-            break;
-        case 'month':
-            for(var c = 0; c < readyMonthArray.length; c++) {
-                renderContent+= '<div class="month-cell" style="position: relative; display: inline-block;width: ' + 100/(readyMonthArray.length) + '%; height: '+ readyMonthArray[c]/30 +'px; background-color:'+ getRandomColor() +';"></div>';
-            }
-            break;
-    }
-    renderTarget.innerHTML = renderContent;
-
-}
